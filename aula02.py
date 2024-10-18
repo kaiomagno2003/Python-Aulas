1ec24b39c1edacfa7a18c58b64ba582d6a2fef78
kaiomagno2003 <kaiomalmeida@gmail.com>
2024-10-17 23:42:00 -0300

    Aula 02 até atividade 4

---
+++ aula02.py
@@ -0,0 +1,36 @@
+# x = 5
+# y = 10
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+# a = True
+# b = False
+# resultado = (a and b)
+# resultado = (a or b)
+# resultado = not a
+
+# idade1 = int(input("Insira a primeira idade: "))
+# idade2 = int(input("Insira a segunda idade: "))
+# print("A primeira idade é maior que a segunda?",idade1 > idade2)
+# print("A primeira idade é menor que a segunda?",idade1 < idade2)
+# print("A primeira idade é igual que a segunda?",idade1 == idade2)
+
+# palavra1 = input("Insira a primeira palavra: ")
+# palavra2 = input("Insira a segunda palavra: ")
+# print("As palavras são iguais?", palavra1 == palavra2)
+
+# idade = int(input("Qual a sua idade? "))
+# habilitado = (input("Possui habilitação? (sim ou não) "))
+# maiorIdade = idade >= 18
+# habilitacao = habilitado == "sim"
+# print(maiorIdade and habilitacao)
+
+# nota1 = float(input("Insira a primeira nota: "))
+# nota2 = float(input("Insira a segunda nota: "))
+# resulNota1 = nota1 >= 6
+# resulNota2 = nota2 >= 6
+# print(resulNota1 and resulNota2)
