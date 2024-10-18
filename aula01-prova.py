1571ddf6821e7bec3ca6f4a5f8172239d389fa73
kaiomagno2003 <kaiomalmeida@gmail.com>
2024-10-17 22:54:54 -0300

    Prova Aula 01

---
+++ aula01-prova.py
@@ -0,0 +1,7 @@
+print("Vamos calcular a área do retângulo?")
+base = float(input("Informe a medida da base do retângulo em centimetros: "))
+altura = float(input("Informe a medida da altura do retângulo em centimetros: "))
+
+area = base * altura
+
+print(f"A área do retângulo é {area}cm")
